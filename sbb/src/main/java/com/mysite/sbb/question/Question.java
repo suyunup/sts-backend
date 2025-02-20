cb8e959d25498f90b79fa1ec3dbcb1a0f5555def
suyun <suyunup@kookmin.ac.kr>
2025-02-21 00:22:19 +0900

    feat: 3장 끝

--- sbb/src/main/java/com/mysite/sbb/question/Question.java
+++ sbb/src/main/java/com/mysite/sbb/question/Question.java
@@ -1,19 +1,22 @@
 package com.mysite.sbb.question; 
 
-import java.time.LocalDateTime; 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.user.SiteUser;
 
-import jakarta.persistence.CascadeType; 
-import jakarta.persistence.Column; 
-import jakarta.persistence.Entity; 
-import jakarta.persistence.GeneratedValue; 
-import jakarta.persistence.GenerationType; 
-import jakarta.persistence.Id; 
-import jakarta.persistence.OneToMany; 
-
-import lombok.Getter; 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
 import lombok.Setter; 
 
 @Getter 
@@ -34,4 +37,12 @@ public class Question {
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) 
     private List<Answer> answerList; 
+    
+    @ManyToOne
+    private SiteUser author;
+    
+    private LocalDateTime modifyDate;
+    
+    @ManyToMany
+    Set<SiteUser> voter;
 }
